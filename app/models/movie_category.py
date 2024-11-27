93a7a4078c2cac924d9389fa63ccc9d1b3edca98
TinaK92 <tinakonopko@gmail.com>
2024-11-26 19:39:29 -0500

    indent error

--- app/models/movie_category.py
+++ app/models/movie_category.py
@@ -13,12 +13,12 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 #     # categories = db.relationship("Category", back_populates="movies")
 
 
-  movie_categories = db.Table(
-    "movie_categories",
-     db.Column("movie_id", db.Integer, db.ForeignKey(add_prefix_for_prod("movies.id")), primary_key=True),
-     db.Column("category_id", db.Integer, db.ForeignKey(add_prefix_for_prod("categories.id")), primary_key=True)
-  )
+movie_categories = db.Table(
+"movie_categories",
+    db.Column("movie_id", db.Integer, db.ForeignKey(add_prefix_for_prod("movies.id")), primary_key=True),
+    db.Column("category_id", db.Integer, db.ForeignKey(add_prefix_for_prod("categories.id")), primary_key=True)
+)
 
-    __tablename__ = "movie_categories"
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+__tablename__ = "movie_categories"
+if environment == "production":
+    __table_args__ = {'schema': SCHEMA}
