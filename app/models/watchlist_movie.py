f1403993105dd97cb831a02bc440e5b25a21a9dd
TinaK92 <tinakonopko@gmail.com>
2024-11-26 21:59:09 -0500

    fixed join table

--- app/models/watchlist_movie.py
+++ app/models/watchlist_movie.py
@@ -1,20 +1,12 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
-# Join Table
-# class WatchlistMovie(db.Model):
-#     __tablename__ = "watchlist_movies"
-#     if environment == "production":
-#         __table_args__ = {'schema': SCHEMA}
-
-
-#     watchlist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('watchlists.id')), primary_key=True)
-#     movie_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('movies.id')), primary_key=True)
-
-#     movie = db.relationship('Movie', back_populates='watchlist_movies')
-#     watchlist = db.relationship('Watchlist', back_populates='watchlist_movies')
 
 watchlist_movies = db.Table(
     "watchlist_movies", 
     db.Column("watchlist_id", db.Integer, db.ForeignKey(add_prefix_for_prod("watchlists.id")), primary_key=True),
     db.Column("movie_id", db.Integer, db.ForeignKey(add_prefix_for_prod("movies.id")), primary_key=True)
 )
+
+__tablename__ = "watchlist_movies"
+if environment == "production":
+    __table_args__ = {'schema': SCHEMA}
