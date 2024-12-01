c949b66cb21367f738ccf9f4b454fa91066e14f4
TinaK92 <tinakonopko@gmail.com>
2024-11-30 19:36:09 -0500

    remove table production stuff

--- app/models/comment.py
+++ app/models/comment.py
@@ -3,9 +3,9 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class Comment(db.Model):
     __tablename__ = 'comments'
 
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
-        
+    # if environment == "production":
+    #     __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     movie_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('movies.id')), nullable=False)
--- app/models/movie_category.py
+++ app/models/movie_category.py
@@ -4,9 +4,9 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class MovieCategory(db.Model):
     __tablename__ = 'movie_categories'
 
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
-        
+    # if environment == "production":
+    #     __table_args__ = {'schema': SCHEMA}
+
     movie_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("movies.id")), primary_key=True)
     category_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("categories.id")), primary_key=True)
 
